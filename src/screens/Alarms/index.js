95cc7500629b1df438650516c056cb1e1848d45a
Enrique <jmtov4@gmail.com>
2020-06-14 22:54:40 -0300

    Alarm Filter - GraphQL Integration - Now alarms can be searched by name or by status. - Added Loading and Errored components to handle request states. - Edited base alarm query to receive name and status filter. - Separated AlarmsList and Alarms screens to avoid innecessary re-renders. - Fixed props on the FilterBar Field components.

--- src/screens/Alarms/index.js
+++ src/screens/Alarms/index.js
@@ -1,42 +1,20 @@
-import React, { useCallback } from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import React, { useCallback, useState } from 'react';
 
-import { GET_ALARMS_QUERY } from 'queries/alarm';
-
-import Alarm from 'components/Alarm';
-
+import AlarmsList from './components/AlarmsList';
 import FilterBar from './components/FilterBar';
 import './styles.scss';
 
 function Alarms() {
-  const { loading, error, data } = useQuery(GET_ALARMS_QUERY);
+  const [filter, setFilters] = useState({});
 
-  const handleFilterChange = useCallback((newFilters) => {
-    console.log(newFilters);
+  const handleFilterChange = useCallback(newFilters => {
+    setFilters(newFilters);
   }, []);
-
-  // TODO: Improve this loading and error rendering
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error :/</h1>;
 
   return (
     <div className="alarms-screen">
       <FilterBar onFilterChange={handleFilterChange} />
-      <div className="alarms__list">
-        {data.alarms.map(alarm => (
-          <Alarm
-            key={alarm.id}
-            id={alarm.id}
-            name={alarm.name}
-            previous_status={alarm.previous_status}
-            source={alarm.source}
-            status={alarm.status}
-            trigger_condition={alarm.trigger_condition}
-            trigger_value={alarm.trigger_value}
-            type={alarm.type}
-          />
-        ))}
-      </div>
+      <AlarmsList nameFilter={filter.nameFilter} statusFilter={filter.statusFilter} />
     </div>
   );
 }
